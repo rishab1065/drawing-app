551f4c3c394eaf9e764b599982ebfea1f121be08
Rishab Jain <rishab1065@gmail.com>
2020-10-28 13:23:28 +0530

    added tooltip

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Toolbar.scss';
-import brush from '../../assets/svgs/draw_tool.svg';
-import highlighter from '../../assets/svgs/highlighter.svg';
-import eraser from '../../assets/svgs/eraser.svg';
 
 function Toolbar(props) {
   const [selectedTool, setSelectedTool] = useState(null);
@@ -14,46 +11,50 @@ function Toolbar(props) {
   return (
     <div className="Toolbar">
       <div className="Tools_Container">
-        <img
-          src={brush}
-          alt="Brush"
-          className="Tool"
+        <div
+          className="Tool Brush Tooltip"
           onClick={() => setSelectedTool('brush')}
-        />
-        <img
-          src={highlighter}
-          alt="Highlighter"
-          className="Tool"
+        >
+          <div className="Tooltip_Text">Brush</div>
+        </div>
+        <div
+          className="Tool Highlighter Tooltip"
           onClick={() => setSelectedTool('highlighter')}
-        />
-        <img
-          src={eraser}
-          alt="Eraser"
-          className="Tool"
+        >
+          <div className="Tooltip_Text">Highlighter</div>
+        </div>
+        <div
+          className="Tool Eraser Tooltip"
           onClick={() => setSelectedTool('eraser')}
-        />
+        >
+          <div className="Tooltip_Text">Eraser</div>
+        </div>
       </div>
       {(selectedTool === 'brush' || selectedTool === 'highlighter') && (
         <div className="SubTools_Container">
           {selectedTool === 'brush' && (
             <>
-              <div className="Brush_Thickness">
+              <div className="Brush_Thickness Tooltip">
                 <div className="Brush_Small" />
+                <div className="Tooltip_Text">1px</div>
               </div>
-              <div className="Brush_Thickness">
+              <div className="Brush_Thickness Tooltip">
                 <div className="Brush_Medium" />
+                <div className="Tooltip_Text">3px</div>
               </div>
-              <div className="Brush_Thickness">
+              <div className="Brush_Thickness Tooltip">
                 <div className="Brush_Large" />
+                <div className="Tooltip_Text">5px</div>
               </div>
             </>
           )}
-          <div className="Tool_Color">
+          <div className="Tool_Color Tooltip">
             <input
               type="color"
               id="html5colorpicker"
               onChange={onColorChange}
             />
+            <div className="Tooltip_Text">Color Picker</div>
           </div>
         </div>
       )}
